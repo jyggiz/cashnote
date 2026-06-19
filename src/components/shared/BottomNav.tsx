af02dd76dbf9043b045e8c686d155d1fcb6eccfb
jyggiz <zhygmytov@gmail.com>
2026-06-19 13:32:04 +0500

    Revert nav to fixed; increase bottom padding to clear nav bar

--- src/components/shared/BottomNav.tsx
+++ src/components/shared/BottomNav.tsx
@@ -42,7 +42,7 @@ const tabs = [
 
 export default function BottomNav() {
   return (
-    <nav className="shrink-0 border-t border-gray-200 bg-white safe-bottom dark:border-gray-800 dark:bg-gray-900">
+    <nav className="fixed bottom-0 left-0 right-0 z-50 border-t border-gray-200 bg-white safe-bottom dark:border-gray-800 dark:bg-gray-900">
       <div className="mx-auto flex max-w-lg">
         {tabs.map(tab => (
           <NavLink
